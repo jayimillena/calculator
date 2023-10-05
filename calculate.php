2cd9b356c3391da4c0f9b5b55859207773d3aeb7
Two <22344029+jayimillena@users.noreply.github.com>
2023-10-05 13:20:44 +0800

    Variables and datatypes

--- calculate.php
+++ calculate.php
@@ -1,4 +1,9 @@
 <?php 
 
-  echo $_POST['first-number'] + $_POST['second-number'];
+  $fNum = $_POST['first-number'];
+  $sNum = $_POST['second-number'];
+
+  $total = $fNum + $sNum;
+
+  echo "The sum of $fNum and $sNum is $total";
 
