98e2dce8ab72b18162181987eb6769027464b192
Two <22344029+jayimillena@users.noreply.github.com>
2023-10-05 12:33:06 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calculator</title>
+</head>
+<body>
+  <form action="calculate.php" method="post">
+    <label for="first_number">First Number: </label>
+    <input type="text" name="first-number" placeholder="First Number" />
+    
+    <label for="seconds_number">Second Number: </label>
+    <input type="text" name="seconds-number" placeholder="Seconds Number" />
+
+    <button type="submit" name="btn-get-sum">Get Sum</button>
+  </form>
+</body>
+</html>
